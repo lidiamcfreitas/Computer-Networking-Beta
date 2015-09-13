a0350d6dec99dcf5886dbbd14e21eecba27c1fdc
lidiamcfreitas <lidiamcfreitas@gmail.com>
2015-09-14 00:24:03 +0100

    added udp server header

---
+++ UDPEchoServer.c
@@ -0,0 +1,9 @@
+//
+//  UDPEchoServer.c
+//  
+//
+//  Created by Lídia Maria Carvalho de Freitas on 14/09/15.
+//
+//
+
+#include "UDPEchoServer.h"
---
+++ UDPEchoServer.h
@@ -0,0 +1,14 @@
+//
+//  UDPEchoServer.h
+//  
+//
+//  Created by Lídia Maria Carvalho de Freitas on 14/09/15.
+//
+//
+
+#ifndef ____UDPEchoServer__
+#define ____UDPEchoServer__
+
+#include <stdio.h>
+
+#endif /* defined(____UDPEchoServer__) */
